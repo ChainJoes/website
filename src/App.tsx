63361faee64266d56e1bebff0730d9b5df31cbba
Magerram Gubadov <magerram.gubadov@niwart.io>
2023-12-19 17:16:47 +0300

    fix hero scroll

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,49 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import {Footer} from "./Components/Footer/Footer";
 import {Sales} from "./Pages/Sales/Sales";
 import {WagmiConfig, createConfig, configureChains} from 'wagmi'
-import {createPublicClient, http} from "viem";
 import { goerli } from 'viem/chains';
+import { publicProvider } from 'wagmi/providers/public';
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { Buffer } from "buffer";
 
-const config = createConfig({
+if (!window.Buffer) {
+  window.Buffer = Buffer;
+}
+
+const { chains, publicClient  } = configureChains(
+  [goerli],
+  [publicProvider()],
+);
+
+const config  = createConfig({
   autoConnect: true,
-  publicClient: createPublicClient({
-    chain: goerli,
-    transport: http()
-  }),
-})
+  publicClient,
+  connectors: [
+      new MetaMaskConnector({ chains }),
+      new CoinbaseWalletConnector({
+          chains,
+          options: {
+              appName: 'wagmi',
+          },
+      }),
+      new WalletConnectConnector({
+          chains,
+          options: {
+              projectId: 'd6310936cc880b14ead9e2652c25f96e',
+          },
+      }),
+      new InjectedConnector({
+          chains,
+          options: {
+              name: 'Injected',
+              shimDisconnect: true,
+          },
+      }),
+  ],
+});
 
 export const App = () => {
   return (
--- src/Pages/Sales/Sections/Hero/SalesForm/SalesForm.tsx
+++ src/Pages/Sales/Sections/Hero/SalesForm/SalesForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Formik, Form } from "formik";
 import ethIcon from './Assets/ethereum.png'
 import { TokensTabs } from "./TokensTabs";
@@ -8,17 +8,42 @@ import './SalesForm.scss'
 import ConnectModal from './ConnectModal';
 import { contractAbi } from './Api/api';
 import { StatusModal } from './StatusPopup';
+import { parseEther } from 'viem';
 
 export const SalesForm = () => {
   const presale_address = '0xf2d17b5701b89811d87c185862e7a1a4c634cea7';
   const { connect, connectors } = useConnect();
   const { isConnected, address } = useAccount();
+  const { data: ethBalance } = useBalance({ address: address });
   const [activeToken, setActiveToken] = useState(0);
   const [showPopup, setShowPopup] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [valueETH, setValueETH] = useState<number | ''>('');
+  const [valueToken, setValueToken] = useState<number | ''>('');
 
+  const handleValueETHChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (rate) {
+      const newValueETH = e.target.value === '' ? '' : Number(e.target.value);
+      setValueETH(newValueETH);
+      setValueToken(newValueETH === '' ? '' : newValueETH * parseFloat(rate?.toString()));
+    } else {
+      setValueETH(0);
+      setValueToken(0);
+    }
+  };
 
-  const { data: rate, isLoading: isRateLoading } = useContractRead({
+  const handleValueTokenChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (rate) {
+      const newValueToken = e.target.value === '' ? '' : Number(e.target.value);
+      setValueToken(newValueToken);
+      setValueETH(newValueToken === '' ? '' : newValueToken / parseFloat(rate?.toString()));
+    } else {
+      setValueETH(0);
+      setValueToken(0);
+    }
+  };
+
+  const { data: rate } = useContractRead({
     address: presale_address,
     abi: contractAbi,
     functionName: 'getCurrentRate',
@@ -42,6 +67,65 @@ export const SalesForm = () => {
     watch: true
   });
 
+  const tokenSalesCount = (Array.isArray(tokenSales) && tokenSales[1] !== undefined) && tokenSales[1].toString()
+
+  function calculatePercentage(firstNumber: number, secondNumber: number): number {
+    if (secondNumber === 0) {
+      return 0;
+    }
+    return (firstNumber / secondNumber) * 100;
+  }
+
+  const progressPercentage = calculatePercentage(
+    parseInt(tokensSold?.toString() ?? "0"),
+    parseInt(tokenSalesCount ?? "0")
+  )
+
+  const { write: buyTokensWithEther, isLoading: isPresaleLoading, isSuccess: isPresaleSuccess, data: txPurchase } = useContractWrite({
+    address: presale_address,
+    abi: contractAbi,
+    functionName: 'buy',
+    account: address
+  });
+
+  const BuyPayAccept = useCallback(async () => {
+    if ((ethBalance && +valueETH > +ethBalance) || valueETH === 0) {
+      //TODO: ADD ALERT
+      return;
+    }
+    try {
+      await buyTokensWithEther({ args: [parseEther(valueETH.toString()), TokensData[activeToken].token.toString()] });
+      //TODO: ADD ALERT
+    } catch (e) {
+    }
+  }, [valueETH, ethBalance, activeToken, buyTokensWithEther]);
+
+  const { data : txData, isSuccess : isTxSuccess, isLoading : isTxLoading, refetch : refreshTxData} = useWaitForTransaction({hash : txPurchase?.hash});
+
+  useEffect(() => {
+    if (isTxLoading) {
+        // toastId.current = toast.info(
+        //     `Processing ${ethAmount * rate.toString() } PAYT`, {
+        //         position: "top-right",
+        //         autoClose: 30000,
+        //         hideProgressBar: false,
+        //         closeOnClick: true,
+        //         pauseOnHover: true,
+        //         draggable: true,
+        //         progress: undefined,
+        //     });
+    }
+
+    if (isTxSuccess) {
+        // toast.update(toastId.current, {
+        //     render: `Successfully bought ${ethAmount * rate.toString() } PAYT`,
+        //     type: toast.TYPE.SUCCESS,
+        //     autoClose: 5000
+        // });
+        refreshTxData();
+    }
+}, [isTxSuccess, isTxLoading]);
+
   return (
     <div className="sales-form">
       <Formik
@@ -64,7 +148,7 @@ export const SalesForm = () => {
                     ETH
                   </div>
                   <div className="rate">
-                    1 ETH ≈ {TokensData[activeToken].rate} {TokensData[activeToken].name}
+                    1 ETH ≈ {rate?.toString()} {TokensData[activeToken].name}
                   </div>
                 </div>
               </div>
@@ -72,7 +156,14 @@ export const SalesForm = () => {
                 You send
               </span>
             </div>
-            <input type="text" name='send-token' id='send-token' disabled={!isConnected} />
+            <input
+              type="number"
+              name='send-token'
+              value={valueETH}
+              onChange={handleValueETHChange}
+              id='send-token'
+              disabled={!isConnected}
+            />
           </label>
           <label className='field' htmlFor="get-token">
             <div className="field__head">
@@ -88,7 +179,14 @@ export const SalesForm = () => {
                 You get
               </span>
             </div>
-            <input type="text" name='get-token' id='get-token' disabled={!isConnected} />
+            <input
+              type="number"
+              name='get-token'
+              value={valueToken}
+              onChange={handleValueTokenChange}
+              id='get-token'
+              disabled={!isConnected}
+            />
           </label>
           {isConnected &&
             <div className="progress">
@@ -102,11 +200,11 @@ export const SalesForm = () => {
                   </div>
                 </div>
                 <div className="progress__percent">
-                  {TokensData[activeToken].progress}%
+                  {progressPercentage}%
                 </div>
               </div>
               <div className="progress__bar">
-                <div className="progress__bar_current" style={{ width: `${TokensData[activeToken].progress}%` }}></div>
+                <div className="progress__bar_current" style={{ width: `${progressPercentage}%` }}></div>
               </div>
             </div>
           }
